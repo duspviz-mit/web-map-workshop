88d237c59e4dc49f6cd7c9878e4ff6ff5f0a3a33
Eric Robsky Huntley <ehuntley@mit.edu>
2019-11-12 17:50:24 -0500

    make query legible

--- 3_webmapFundamentals/prep_scripts/fetch_obs_bike_lanes.R
+++ 3_webmapFundamentals/prep_scripts/fetch_obs_bike_lanes.R
@@ -1,9 +1,13 @@
 require('dplyr')
 require('sf')
 
-query = "https://data.cambridgema.gov/resource/2z9k-mv9g.csv?$where=ticket_created_date_time>'2019-06-01T12:00:00'&issue_type=Bike%20Lane%20Obstruction"
+query = paste0("https://data.cambridgema.gov/resource/2z9k-mv9g.csv?$where=",
+  "ticket_created_date_time>'2019-06-01T12:00:00'", 
+  "&" , 
+  "issue_type=Bike%20Lane%20Obstruction"
+)
 
-cam_service <- read.csv(query, stringsAsFactors = FALSE) %>%
+read.csv(query, stringsAsFactors = FALSE) %>%
   st_as_sf(coords =  c('lng', 'lat')) %>%
   rename(created = ticket_created_date_time) %>%
   select(c('ticket_id', 'ticket_status', 'created', 'address', 'geometry')) %>%
