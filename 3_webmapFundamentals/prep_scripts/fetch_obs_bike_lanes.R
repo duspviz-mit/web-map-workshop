bcdaedc1380a45b632740fb164119e7cb7c37d2b
Eric Robsky Huntley <ehuntley@mit.edu>
2019-11-12 17:51:56 -0500

    match file names

--- 3_webmapFundamentals/prep_scripts/fetch_obs_bike_lanes.R
+++ 3_webmapFundamentals/prep_scripts/fetch_obs_bike_lanes.R
@@ -11,4 +11,4 @@ read.csv(query, stringsAsFactors = FALSE) %>%
   st_as_sf(coords =  c('lng', 'lat')) %>%
   rename(created = ticket_created_date_time) %>%
   select(c('ticket_id', 'ticket_status', 'created', 'address', 'geometry')) %>%
-  st_write('~/Desktop/obstructed_bike_lanes.geojson')
+  st_write('obs_bike_lanes.geojson')
